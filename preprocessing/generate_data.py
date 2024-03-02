b94a36f384c8c62188db60b5254c2f708f88d4bd
HenryG <greeht01@pfw.edu>
2024-03-01 21:56:19 -0500

    Separated all data into stock and currency in date range

--- preprocessing/generate_data.py
+++ preprocessing/generate_data.py
@@ -2,82 +2,104 @@
 import os
 import zipfile
 import pandas
-import numpy
 
+# pylint: disable=locally-disabled, line-too-long
 
-#Unzip the data/grab the data? X
-#Grab relevant data and put it into a dataframe
-#Calculate new features from data
-#Add new features to the dataframe
-#Save dataframe as CSV
+# Unzip the data/grab the data? X
+# Grab relevant data and put it into a dataframe
+# Calculate new features from data
+# Add new features to the dataframe
+# Save dataframe as CSV
 
-#Unzipping data
-#Exchange rate dataset: https://www.kaggle.com/datasets/dhruvildave/currency-exchange-rates
-#Stock Market Dataset: https://www.kaggle.com/datasets/borismarjanovic/price-volume-data-for-all-us-stocks-etfs/data
+# Unzipping data
+# Exchange rate dataset: https://www.kaggle.com/datasets/dhruvildave/currency-exchange-rates
+# Stock Market Dataset: https://www.kaggle.com/datasets/borismarjanovic/price-volume-data-for-all-us-stocks-etfs/data
 
-#NOTE: You will need to rename the stock/currency data zip files to these names specifically for this script to function.
+# NOTE: You will need to rename the stock/currency data zip files to these names specifically for this script to function.
 STOCK_ZIP_NAME = "stock.zip"
 CURRENCY_ZIP_NAME = "currency.zip"
 
 STOCK_PREFIX = os.getcwd() + "\\preprocessing\\intermediate\\stock\\Data\\Stocks\\"
 CURRENCY_PREFIX = os.getcwd() + "\\preprocessing\\intermediate\\currency\\"
 
-#Check if stock data has already been unzipped
+# Check if stock data has already been unzipped
 if not os.path.isfile(os.path.join(STOCK_PREFIX + "a.us.txt")):
     print("Stock data not found, begin unzipping.")
     with zipfile.ZipFile(os.path.join(os.getcwd() + "\\preprocessing\\data\\" + STOCK_ZIP_NAME), 'r') as zippedfile:
-        zippedfile.extractall(os.path.join(os.getcwd() + "\\preprocessing\\intermediate\\stock"))
+        zippedfile.extractall(os.path.join(
+            os.getcwd() + "\\preprocessing\\intermediate\\stock"))
     print("Finished unzipping stock data.")
 
-#Check if currency data has already been unzipped
+# Check if currency data has already been unzipped
 if not os.path.isfile(os.path.join(CURRENCY_PREFIX + "forex.csv")):
     print("Currency file not found, begin unzipping.")
     with zipfile.ZipFile(os.path.join(os.getcwd() + "\\preprocessing\\data\\" + CURRENCY_ZIP_NAME), 'r') as zippedfile:
-        zippedfile.extractall(os.path.join(os.getcwd() + "\\preprocessing\\intermediate\\currency"))
+        zippedfile.extractall(os.path.join(
+            os.getcwd() + "\\preprocessing\\intermediate\\currency"))
     print("Finished unzipping currency data.")
 
+###
+# Converting currency data to dataframes
+###
 
+CURRENCIES_TO_GRAB = [
+    "USD/EUR",
+    "USD/JPY",
+    "USD/AUD",
+    "USD/CAD"]  # Means rate of USD to X currency
 
-#Grabbing relevant data for dataframe.
-STOCKS_TO_GRAB = ["AAPL", "GOOG", "MSFT", "AMZN"] # Limited list for testing
-CURRENCIES_TO_GRAB = ["USD/EUR", "USD/JPY", "USD/AUD", "USD/CAD"] # Means rate of USD to X currency
-
-#NOTE: Date ranges in currency dataset:
+# NOTE: Date ranges in currency dataset:
 # USD/CAD - 2003/09/17 - 2021/08/30
 # USD/EUR - 2003/12/01 - 2021/08/30
 # USD/JPY - 1996/10/30 - 2021/08/30
 # USD/AUD - 2006/05/16 - 2021/08/30
 
-#Convert stock names into their file locations
+# Turn currency csv into dataframe
+currency_df = pandas.read_csv(CURRENCY_PREFIX + "forex.csv")
+currency_df['date'] = pandas.to_datetime(currency_df['date'])
+
+# Define date range
+START_DATE = '2006-05-16'
+END_DATE = '2010-01-01'
+
+# Create a boolean mask for rows within the date range
+currency_date_mask = (currency_df['date'] >= START_DATE) & (currency_df['date'] <= END_DATE)
+intermediate_currency_df = currency_df[currency_date_mask]
+
+# Boolean mask for separating into different currencies
+currency_frame_list = []
+for currency in CURRENCIES_TO_GRAB:
+    mask = intermediate_currency_df['slug'] == currency
+    currency_frame_list.append(intermediate_currency_df[mask])
+
+for c in currency_frame_list:
+    print(c.head())
+
+###
+# Converting stock data to dataframe
+###
+
+# Convert stock names into their file locations
+STOCKS_TO_GRAB = ["AAPL", "GOOGL", "MSFT", "AMZN"]  # Limited list for testing
+
 stock_files = []
 for stock in STOCKS_TO_GRAB:
     stock_files.append(STOCK_PREFIX + stock + ".us.txt")
 
-#Turn stockfiles into dataframes
+
+# Turn stockfiles into dataframes
 stock_frame_list = []
 for file in stock_files:
     df = pandas.read_csv(file)
-    stock_frame_list.append(df)
+    df['Date'] = pandas.to_datetime(df['Date'])
+    df.rename(columns={'Date' : 'date'}, inplace=True)
+    df.drop(columns=['OpenInt'], inplace=True)
+    stock_date_mask = (df['date'] >= START_DATE) & (df['date'] <= END_DATE)
+    stock_frame_list.append(df[stock_date_mask])
 
-#Turn currency csv into dataframe
-currency_df = pandas.read_csv(CURRENCY_PREFIX + "forex.csv")
+for stock in stock_frame_list:
+    print(stock.head())
 
-currency_df['date'] = pandas.to_datetime(currency_df['date'])
-
-# currency_df = currency_df.set_index(['date'])
-
-# Define your date range
-start_date = '2004-11-08'
-end_date = '2005-02-18'
-
-# Create a boolean mask for rows within the date range
-mask = (currency_df['date'] >= start_date) & (currency_df['date'] <= end_date)
-
-# Apply the mask to filter rows within the date range
-rows_within_range = currency_df[mask]
-
-# Print or further process the filtered rows
-print(rows_within_range)
-# print(currency_df.loc['2004-11-08' : '2005-02-18'])
-
-#Concatendate all dataframes
+###
+# Concatendate all dataframes
+###
