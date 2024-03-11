031fbbdb591b5b0512eb15f1044df3a40b254c5b
HenryG <greeht01@pfw.edu>
2024-03-10 22:44:45 -0400

    Method prototyping

---
+++ online_learning/online-learning.py
@@ -0,0 +1,14 @@
+
+
+def learn(model):
+    '''Calls API for recent stock data, saves the current
+    state of the model, trains the model on the new data, 
+    and then saves the newly created model.'''
+    #TODO: Save the current state of the model
+
+    #TODO: Call API for new data
+    new_data = []
+
+    #TODO: Train the model on API data
+
+    #TODO: Save new model state
