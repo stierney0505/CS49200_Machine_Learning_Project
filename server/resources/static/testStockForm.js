8dede7d5d91e3ea7a410514fc63a2bada145173e
lukebushur <luke7ajb@gmail.com>
2024-03-12 00:03:02 -0400

    WIP use machine learning model for front-end

--- server/resources/static/testStockForm.js
+++ server/resources/static/testStockForm.js
@@ -7,7 +7,34 @@ document.getElementById("stockForm").addEventListener("submit", function(event) 
     })
     .then(response => response.text())
     .then(data => {
-        document.getElementById("result").innerText = data;
+        dataObj = JSON.parse(data);
+        if ((new Date(dataObj.timestamp)) > new Date()) {
+            document.getElementById('result').innerText = dataObj;
+        }
+        console.log(dataObj);
+        const table = document.createElement('table');
+        const thead = table.createTHead();
+        const headRow = thead.insertRow(0);
+        const tbody = table.createTBody();
+        const bodyRow = tbody.insertRow(0);
+        for (const [key, value] of Object.entries(dataObj)) {
+            const th = document.createElement('th');
+            th.innerHTML = key.charAt(0).toUpperCase() + key.slice(1);
+            headRow.appendChild(th);
+
+            const td = bodyRow.insertCell();
+            if (key === 'timestamp') {
+                td.innerHTML = (new Date(value[0])).toDateString();
+            } else {
+                td.innerHTML = value[0];
+            }
+        }
+        document.getElementById('result').innerHTML = '';
+        const resultStr = document.createElement('h2');
+        resultStr.innerHTML = 'Result:'
+
+        document.getElementById('result').appendChild(resultStr);
+        document.getElementById('result').appendChild(table);
     })
     .catch(error => {
         console.error('Error:', error);
--- server/routes/serverExampleRoutes.py
+++ server/routes/serverExampleRoutes.py
@@ -1,5 +1,43 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
 import os
+from dotenv import load_dotenv
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame
+from datetime import date, datetime, timedelta
+
+import keras
+print(keras.__version__)
+
+load_dotenv()
+
+client = StockHistoricalDataClient(os.getenv('API_KEY'), os.getenv('API_SECRET'))
+
+def stock_info_from_range(symbols: list[str], start: datetime, end: datetime):
+    request_params = StockBarsRequest(
+        symbol_or_symbols=symbols,
+        timeframe=TimeFrame.Day,
+        start=start,
+        end=end
+    )
+
+    apple_bars = client.get_stock_bars(request_params)
+
+    return apple_bars.df
+
+def stock_info_from_day(symbols: list[str], day: datetime):
+    end = day + timedelta(days=1)
+
+    request_params = StockBarsRequest(
+        symbol_or_symbols=symbols,
+        timeframe=TimeFrame.Day,
+        start=day,
+        end=end
+    )
+
+    apple_bars = client.get_stock_bars(request_params)
+
+    return apple_bars.df
 
 serverExample_bp = Blueprint('serverExample', __name__, url_prefix='/serverExample')
 
@@ -10,5 +48,19 @@ def helloworld():
 
 @serverExample_bp.route('/predictStockData', methods=['POST'])
 def useMLModel():
-    returnDict = {"just": "kidding"}
-    return returnDict
+    stock = request.form.get('stock_ticker')
+    date_str = request.form.get('date')
+    date_input = datetime.strptime(date_str, '%Y-%m-%d')
+    if date_input < date_input.today():
+        prices = stock_info_from_day([stock], date_input)
+        prices.drop(['trade_count', 'vwap'], axis=1, inplace=True)
+        return prices.reset_index().to_json()
+    else:
+        prices = stock_info_from_range([stock], date.today() - timedelta(weeks=15), date.today())
+        prices.drop(['trade_count', 'vwap'], axis=1, inplace=True)
+        prices_formatted = prices.reset_index().drop(['symbol'], axis=1);
+        print(prices_formatted.shape)
+
+        model = keras.models.load_model('./resources/aapl_lstm_model.h5')
+        
+        return prices.reset_index().to_json()
