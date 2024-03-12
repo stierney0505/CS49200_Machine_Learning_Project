034cb472e55087f79065a8270017c82e6ee48b8a
HenryG <greeht01@pfw.edu>
2024-03-12 03:15:21 -0400

    Moved file for easier pathing, updated scaling of API data

---
+++ api_wrapper/online_learning/online-learning.py
@@ -0,0 +1,46 @@
+import datetime
+import os
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+from api_wrapper.get_stock_info import stock_info_from_range
+
+def do_online_learn(ticker: str):
+    '''Calls API for recent stock data, saves the current
+    state of the model, trains the model on the new data, 
+    and then saves the newly created model.'''
+    #Load existing model
+    try:
+        model_path = os.path.join('..', '..', 'model_files', ticker + '_lstm_model_recent.h5')
+        model = keras.models.load_model(model_path)
+    except OSError:
+        model_path = os.path.join('..', '..', 'model_files', ticker + '_lstm_model.h5')
+        model = keras.models.load_model(model_path)
+
+    #Call API for stocks
+    today = datetime.datetime.now()
+
+    new_stock = stock_info_from_range([ticker], today, today - datetime.timedelta(50))
+
+    #TODO: Call API for currency
+
+    #Scale API data
+    cols_to_keep = ["open", "high", "low", "close", "volume", "trade_count"]
+
+    df = new_stock.filter(cols_to_keep)
+    sc = MinMaxScaler(feature_range = (0, 1))
+    df = df.fit_transform(sc)
+
+    array_data = df.values
+    reshaped_array = array_data.reshape(1, 50, 6)
+    df.iloc[:, :] = reshaped_array
+
+    #Train the model on API data
+
+    model.train_on_batch()
+
+    #Save new model state
+    updated_model_name = ticker + '_lstm_model_recent.h5'
+    model.save(updated_model_name)
