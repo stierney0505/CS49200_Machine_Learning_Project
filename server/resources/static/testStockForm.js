840d1ded004708bd9b82cae33cc12ecf0c67ddd0
lukebushur <luke7ajb@gmail.com>
2024-03-12 02:53:57 -0400

    Fixed bugs and enabled all 5 models to be used

--- server/resources/static/testStockForm.js
+++ server/resources/static/testStockForm.js
@@ -7,14 +7,26 @@ document.getElementById("stockForm").addEventListener("submit", function(event) 
     })
     .then(response => response.text())
     .then(data => {
+        // Error getting stock information due to invalid ticker
+        if (data === 'Invalid stock ticker') {
+            document.getElementById('result').innerHTML = '';
+            document.getElementById('result').innerText = 'Error: ' + data;
+            return;
+        }
         dataObj = JSON.parse(data);
+        // The output for a future date is an array with the following information:
+        // symbol, timestamp, open price, high price, low price, close price, volume, and trade count 
         if (dataObj instanceof Array) {
             const headings = ['Symbol', 'Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume', 'Trade Count'];
+
+            // Create a table with the information
             const table = document.createElement('table');
             const thead = table.createTHead();
             const headRow = thead.insertRow(0);
+
             const tbody = table.createTBody();
             const bodyRow = tbody.insertRow(0);
+
             document.getElementById('result').innerText = dataObj;
             for (let i = 0; i < dataObj.length; i++) {
                 const th = document.createElement('th');
@@ -22,13 +34,18 @@ document.getElementById("stockForm").addEventListener("submit", function(event) 
                 headRow.appendChild(th);
 
                 const td = bodyRow.insertCell();
-                if (i === 1) {
-                    td.innerHTML = (new Date(dataObj[1])).toDateString();
+                if (i === 0) {
+                    td.innerHTML = dataObj[i].toUpperCase();
+                } else if (i === 1) {
+                    let startDate = new Date(dataObj[i]);
+                    startDate = new Date(startDate.getTime() + (startDate.getTimezoneOffset() * 60000));
+                    td.innerHTML = startDate.toDateString();
                 } else {
                     td.innerHTML = Math.round(dataObj[i] * 100) / 100;
                 }
             }
             document.getElementById('result').innerHTML = '';
+            document.getElementById('result').innerText = '';
             const resultStr = document.createElement('h2');
             resultStr.innerHTML = 'Result:'
 
@@ -36,6 +53,7 @@ document.getElementById("stockForm").addEventListener("submit", function(event) 
             document.getElementById('result').appendChild(table);
             return;
         }
+        // Create a table with the data from the Alpaca API
         const table = document.createElement('table');
         const thead = table.createTHead();
         const headRow = thead.insertRow(0);
@@ -43,7 +61,11 @@ document.getElementById("stockForm").addEventListener("submit", function(event) 
         const bodyRow = tbody.insertRow(0);
         for (const [key, value] of Object.entries(dataObj)) {
             const th = document.createElement('th');
-            th.innerHTML = key.charAt(0).toUpperCase() + key.slice(1);
+            if (key === 'trade_count') {
+                th.innerHTML = 'Trade Count';
+            } else {
+                th.innerHTML = key.charAt(0).toUpperCase() + key.slice(1);
+            }
             headRow.appendChild(th);
 
             const td = bodyRow.insertCell();
@@ -54,6 +76,7 @@ document.getElementById("stockForm").addEventListener("submit", function(event) 
             }
         }
         document.getElementById('result').innerHTML = '';
+        document.getElementById('result').innerText = '';
         const resultStr = document.createElement('h2');
         resultStr.innerHTML = 'Result:'
 
--- server/routes/serverExampleRoutes.py
+++ server/routes/serverExampleRoutes.py
@@ -46,41 +46,48 @@ def helloworld():
 
 @serverExample_bp.route('/predictStockData', methods=['POST'])
 def useMLModel():
+    # Get form data
     stock = request.form.get('stock_ticker')
     date_str = request.form.get('date')
     date_input = datetime.strptime(date_str, '%Y-%m-%d')
-    print(datetime.today())
-    print(date_input)
+
+    # Any date in the past -> output the date's stock information using Alpaca API
     if date_input.date() < date.today():
         prices = stock_info_from_day([stock], date_input)
         prices.drop(['vwap'], axis=1, inplace=True)
         return prices.reset_index().to_json()
+    # Any date in the future -> predict using ML
     else:
+        # Check that the stock is on the allowed list
+        allowed_stocks = ['aapl', 'amd', 'amzn', 'msft', 'nvda']
+        if stock.lower() not in allowed_stocks:
+            return 'Invalid stock ticker'
+
         import keras
         from sklearn.preprocessing import MinMaxScaler
 
+        # Get the stock information from the past 50 days 
+        # open, high, low, close, volume
         prices = stock_info_from_range([stock], date.today() - timedelta(weeks=15), date.today())
         prices.drop(['vwap'], axis=1, inplace=True)
         prices_formatted = prices.reset_index().drop(['symbol', 'timestamp'], axis=1)
         prices_formatted = prices_formatted[-50:]
-        # print(prices_formatted.shape)
-        # print(prices_formatted)
 
-        model = keras.models.load_model('./resources/aapl_lstm_model.keras')
+        # Load the model
+        model = keras.models.load_model('./resources/' + stock.lower() + '_lstm_model.keras')
 
+        # Scale the stock data
         scaler = MinMaxScaler(feature_range=(0, 1))
         df = scaler.fit_transform(prices_formatted)
-        # print(df)
-
         data_arr = np.array(df)
         reshaped_arr = data_arr.reshape(1, 50, 6)
-        # print(reshaped_arr.shape)
         
+        # Predict using the ML model
         prediction = model.predict(reshaped_arr)
-        unscaled = scaler.inverse_transform(prediction)
-        print(unscaled.shape)
 
-        outputList = [stock, date_input]
+        # Unscale the data and return the data as a response
+        unscaled = scaler.inverse_transform(prediction)
+        outputList = [stock, date_str]
         outputList.extend(unscaled[0].tolist())
 
         return outputList
