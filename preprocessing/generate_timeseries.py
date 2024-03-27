23375c11ff3d7ddd75df72277395f1301fe04040
HenryG <greeht01@pfw.edu>
2024-03-26 23:29:50 -0400

    Timeseries part 1

--- preprocessing/generate_timeseries.py
+++ preprocessing/generate_timeseries.py
@@ -2,6 +2,7 @@ import csv
 import os
 import pandas as pd
 import numpy as np
+import keras
 
 # Convert stock/currency data into dataframes
 # Convert to shape of (Stock, Date, Features)
@@ -10,7 +11,7 @@ import numpy as np
 def read_dataframes_from_csv(files):
     """Convert a list of CSV files into a list of dataframes that
     only contain the columns ['timestamp', 'open', 'high', 'low', 'close']"""
-    dataframes = {}
+    dataframes = []
     for file in files:
         if os.path.exists(file):
             df = pd.read_csv(file)
@@ -24,17 +25,34 @@ def read_dataframes_from_csv(files):
             # Add a new column for Stock/Currency
             df['Stock/Currency'] = stock_name
             # Reorder columns
-            df = df[['Stock/Currency', 'timestamp', 'open', 'high', 'low', 'close']]
-            dataframes[stock_name] = df
+            df = df[[ 'timestamp', 'open', 'high', 'low', 'close']]
+            dataframes.append(df)
         else:
             print(f"Warning: {file} does not exist. Skipping.")
     return dataframes
 
+def df_list_to_time_series(dfs):
+    """Convert a list of dataframes into an array
+    where each element is a timeseries based on the 
+    timestamp column (0) """
+    timeseries_list = []
+    for df in dfs:
+        time_df = keras.utils.timeseries_dataset_from_array(
+            data=df,
+            targets=df['close'],
+            sequence_length=1, #Because the data is 1 day per step in the dataset? Maybe? 
+            batch_size=32
+        )
+        timeseries_list.append(time_df)
+
+    return timeseries_list
+
 csv_files = ['AAPL.csv', 'AMD.csv', 'AMZN.csv', 'aud.csv', 'cad.csv', 'cny.csv', 'eur.csv', 'jpy.csv', 'MSFT.csv', 'NVDA.csv']
 
-dataframes = read_dataframes_from_csv(csv_files)
+df_list = read_dataframes_from_csv(csv_files)
 
-# Concatenate all dataframes into a single dataframe
-combined_df = pd.concat(dataframes.values(), ignore_index=True)
+timeseries = df_list_to_time_series(df_list)
 
-combined_df.to_csv('out.csv', index=False)
+timeseries_array = np.asanyarray(timeseries)
+
+print(timeseries_array[0])
